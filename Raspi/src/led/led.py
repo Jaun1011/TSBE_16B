70e1fdbe113f8adb90e34d7b149951e18c33a2c2
jan <jan.kuonen.93@gmail.com>
2017-05-19 23:17:56 +0200

    add python script

--- Raspi/src/led/led.py
+++ Raspi/src/led/led.py
@@ -1,13 +1,8 @@
 import time
 import RPi.GPIO as GPIO
 
-# Pin-Nummern wie auf dem Raspberry Board verwenden
 GPIO.setmode(GPIO.BOARD)
-
-# Pin 11 (GPIO 17) als Output
 GPIO.setup(11, GPIO.OUT)
-
-# Dauersschleife für das Blinken
 while 1:
     # LED aus
     GPIO.output(11, GPIO.LOW)
